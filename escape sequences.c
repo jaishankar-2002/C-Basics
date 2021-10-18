73d53a2cab88806a3a59368a71d51d0783f017e2
jaishankar-2002 <92287809+jaishankar-2002@users.noreply.github.com>
2021-10-18 10:59:39 +0530

    Add files via upload

---
+++ escape sequences.c
@@ -0,0 +1,27 @@
+/*****************************
+  to use new line- \n  
+  to use new tab- \t  
+  to use carriage return- \r 
+  to use backslash- \\
+  to use double quote- \" 
+  to use vertical tab- \v 
+  to use audible bell- \a 
+  
+  ***************************/
+
+
+#include<stdio.h>
+int main()
+{
+	printf("hello \n world \t my \t name is \"jai shankar\"\n");
+    printf("it\ris  \n");
+    printf("it\rrise   \n");
+    printf("seeker\rall\n");
+    printf("it\ris\rsoak\n");
+    
+    printf("the symbol of backslash is \\");
+    
+    printf("\n  jai \v shankar ");
+    
+    printf("\n\nto get an audible bell \a\a\a\a\a");	
+}
